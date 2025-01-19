d5ada925d74ccd73331e87d2053ebd9e668dbc7f
Ayan Alam <ayanalam0025@gmail.com>
2025-01-19 13:06:59 +0530

    update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -81,7 +81,7 @@ export default function Home() {
                 }
               }}
             >
-              <div className="flex flex-row gap-4">
+              <div className="flex flex-col sm:flex-row gap-4">
                 <div className="container flex flex-col">
                   <Dropdown
                     name="languageFrom"
